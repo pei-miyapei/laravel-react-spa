a525fec483b064e3afb472705797a90f378f88bd
pei-miyapei <pei.miyapei@gmail.com>
2022-01-27 07:55:43 +0000

    Layout.tsx

--- client/src/routes/Router.tsx
+++ client/src/routes/Router.tsx
@@ -1,15 +1,18 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { About } from '../views/About';
 import { Home } from '../views/Home';
+import { Layout } from '../views/Layout';
 import { NotFound } from '../views/NotFound';
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='*' element={<NotFound />} />
+        <Route path='/' element={<Layout />}>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='*' element={<NotFound />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
