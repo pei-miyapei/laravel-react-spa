20251004593efcf1b699bfa19d9b0ad81cf30b0c
pei-miyapei <pei.miyapei@gmail.com>
2022-01-30 22:40:05 +0000

    AuthProvider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,8 @@
 import './App.css';
 import { Router } from './routes/Router';
-import { AuthProvider } from './store/AuthContext';
 
 function App() {
-  return (
-    <AuthProvider>
-      <Router />
-    </AuthProvider>
-  );
+  return <Router />;
 }
 
 export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'antd/dist/antd.css';
+import { AuthProvider } from './store/AuthContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
